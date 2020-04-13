3b59ae93f85d66953f7fa6438a21700dd68774c7
bmbmb <bmbmb@me.com>
2020-04-13 13:32:59 +0100

    reorganised grid so result cells are children of every 5th cell in every row

--- scripts/app.js
+++ scripts/app.js
@@ -14,7 +14,6 @@ function init() {
   // ---> Submit code
   const submitCodeBtn = document.querySelector('.submit-code')
 
-
   //* Grid Vars
   // ---> UserCode Grid
   const cellsCode = []
@@ -30,19 +29,18 @@ function init() {
   const rowsGameBoard = []
   const cellsGameBoard = []
   const cellsResultBox = []
-  const cellsForResultBox = []  // ---> array with last cell of every line
+  const resultCells = []
   const widthGameBoard = 5
   const cellCountGameBoard = widthGameBoard
   const widthGameBoardResult = 2
   const cellCountGameBoardResult = widthGameBoardResult * 2
-
   //---> gameBoard rows
 
   //* Code Vars
   // ---> array for Random code
   const arrayOfCodeParts = ['code-part-a', 'code-part-b', 'code-part-c', 'code-part-d']
   const randomCode = arrayOfCodeParts // ---> array of classes of Random code
-
+  const codeAnswer = []
   let playerCodePosition = 0
 
 
@@ -59,46 +57,62 @@ function init() {
       cellsUserCode[startingPosition].classList.add('code-part-selected')
     }
     // ---> create the Code
-    for (let i = 0; i < cellCountCode; i++) {
+    for (let a = 0; a < cellCountCode; a++) {
       const cellC = document.createElement('div')
-      cellC.textContent = 'C' + i
+      cellC.textContent = 'C' + a
       theCode.appendChild(cellC)
       cellsCode.push(cellC)
     }
     // ---> creates gameBoard grid: 10 rows
-    for (let i = 0; i < 10; i++) {
+    for (let b = 0; b < 10; b++) {
       const row = document.createElement('div')
       gameBoard.appendChild(row)
       rowsGameBoard.push(row)
       row.classList.add('row')
-      row.id = 'row' + (10 - i)
-      for (let i = 0; i < cellCountGameBoard; i++) {
+      row.id = (`row${10 - b}`)
+      for (let c = 0; c < cellCountGameBoard; c++) {
         const cellGB = document.createElement('div')
-        cellGB.textContent = 'G' + i
+        cellGB.textContent = 'G' + c
+        cellGB.classList.add(`row${10 - b}`)
         row.appendChild(cellGB)
         cellsGameBoard.push(cellGB)
-        cellGB.dataset.cellNum = i + 1
+        cellGB.dataset.cellNum = c + 1
         cellGB.classList.add('game-board-empty')
+
+        if (cellGB.dataset.cellNum === '5') {
+          resultCells.push(cellGB)
+          cellGB.classList.remove('game-board-empty')
+          cellGB.classList.add('results')
+          cellGB.textContent = ''
+          for (let d = 0; d < cellCountGameBoardResult; d++) {
+            const cellR = document.createElement('div')
+            cellR.textContent = d
+            cellGB.appendChild(cellR)
+            cellsResultBox.push(cellR)
+          }
+        }
       }
     }
-    // ---> gets last div of every line
-    cellsGameBoard.filter((cellGB) => {
-      if (cellGB.dataset.cellNum === '5') {
-        cellsForResultBox.push(cellGB)
-        cellGB.classList.remove('game-board-empty')
-        cellGB.classList.add('results')
-        cellGB.textContent = ''
-      }
-    })
-    // --> creates a grid inside last div of every line
-    cellsForResultBox.forEach((cell) => {
-      for (let i = 0; i < cellCountGameBoardResult; i++) {
-        const cellR = document.createElement('div')
-        cellR.textContent = i
-        cell.appendChild(cellR)
-        cellsResultBox.push(cellR)
-      }
-    })
+    console.log(resultCells)
+    console.log(cellsResultBox)
+    // // ---> gets last div of every line
+    // cellsGameBoard.filter((cellGB) => {
+    //   if (cellGB.dataset.cellNum === '5') {
+    //     cellsForResultBox.push(cellGB)
+    //     cellGB.classList.remove('game-board-empty')
+    // cellGB.classList.add('results')
+    //     cellGB.textContent = ''
+    //   }
+    // })
+    // // --> creates a grid inside last div of every line
+    // cellsForResultBox.forEach((cell) => {
+    //   for (let d = 0; d < cellCountGameBoardResult; d++) {
+    //     const cellR = document.createElement('div')
+    //     cellR.textContent = d
+    //     cell.appendChild(cellR)
+    //     cellsResultBox.push(cellR)
+    //   }
+    // })
     handleRandomCode(arrayOfCodeParts)
   }
 
@@ -167,12 +181,16 @@ function init() {
 
   // ---> paints userCode with code (TESTING)
   function paintUserCodeGrid() {
+    // for (let i = 0; i < arrayOfCodeParts.length; i++) {
+    //   cellsUserCode[i].classList.add(arrayOfCodeParts[i])
+    // }
     cellsUserCode[0].classList.add('code-part-a')
     cellsUserCode[1].classList.add('code-part-b')
     cellsUserCode[2].classList.add('code-part-c')
     cellsUserCode[3].classList.add('code-part-d')
   }
 
+  let numberOfSubmits = 0
   function handleSubmitCode() {
     const submittedCode = [] // ---> array of classes of Submitted code
     cellsUserCode.forEach(cell => {
@@ -180,26 +198,29 @@ function init() {
       submittedCode.push(cell.className)
     })
 
-    if (cellsGameBoard[45].classList.contains('game-board-empty')) {
-      cellsGameBoard[45].classList.remove('game-board-empty')
-      cellsGameBoard[45].classList.add(cellsUserCode[0].className)
-      cellsGameBoard[46].classList.remove('game-board-empty')
-      cellsGameBoard[46].classList.add(cellsUserCode[1].className)
-      cellsGameBoard[47].classList.remove('game-board-empty')
-      cellsGameBoard[47].classList.add(cellsUserCode[2].className)
-      cellsGameBoard[48].classList.remove('game-board-empty')
-      cellsGameBoard[48].classList.add(cellsUserCode[3].className)
-    } else if (cellsGameBoard[40].classList.contains('game-board-empty')) {
-      cellsGameBoard[40].classList.remove('game-board-empty')
-      cellsGameBoard[40].classList.add(cellsUserCode[0].className)
-      cellsGameBoard[41].classList.remove('game-board-empty')
-      cellsGameBoard[41].classList.add(cellsUserCode[1].className)
-      cellsGameBoard[42].classList.remove('game-board-empty')
-      cellsGameBoard[42].classList.add(cellsUserCode[2].className)
-      cellsGameBoard[43].classList.remove('game-board-empty')
-      cellsGameBoard[43].classList.add(cellsUserCode[3].className)
-    }
-    const codeAnswer = []
+
+
+
+    // if (cellsGameBoard[45].classList.contains('game-board-empty')) {
+    //   cellsGameBoard[45].classList.remove('game-board-empty')
+    //   cellsGameBoard[45].classList.add(cellsUserCode[0].className)
+    //   cellsGameBoard[46].classList.remove('game-board-empty')
+    //   cellsGameBoard[46].classList.add(cellsUserCode[1].className)
+    //   cellsGameBoard[47].classList.remove('game-board-empty')
+    //   cellsGameBoard[47].classList.add(cellsUserCode[2].className)
+    //   cellsGameBoard[48].classList.remove('game-board-empty')
+    //   cellsGameBoard[48].classList.add(cellsUserCode[3].className)
+    // } else if (cellsGameBoard[40].classList.contains('game-board-empty')) {
+    //   cellsGameBoard[40].classList.remove('game-board-empty')
+    //   cellsGameBoard[40].classList.add(cellsUserCode[0].className)
+    //   cellsGameBoard[41].classList.remove('game-board-empty')
+    //   cellsGameBoard[41].classList.add(cellsUserCode[1].className)
+    //   cellsGameBoard[42].classList.remove('game-board-empty')
+    //   cellsGameBoard[42].classList.add(cellsUserCode[2].className)
+    //   cellsGameBoard[43].classList.remove('game-board-empty')
+    //   cellsGameBoard[43].classList.add(cellsUserCode[3].className)
+    // }
+
     for (let i = 0; i < submittedCode.length; i++) {
       if (submittedCode[i] === randomCode[i]) {
         codeAnswer.push('code-right')
@@ -208,6 +229,7 @@ function init() {
       }
     }
 
+    numberOfSubmits++
   }
 
   // --->  Creates userCode and gameBoard grids
